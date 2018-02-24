c3dca3ba7c7a341a2c61f44b3b1dd491f7f51e10
Gino Chiffi <gino.chiffi@gmail.com>
2018-02-24 11:50:20 +0100

    devise, user and shop setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  devise_for :brands,  controllers: {sessions: 'brands/sessions'}
+  devise_for :users,  controllers: {sessions: 'users/sessions' }
+
+  root 'pages#home'
+  get 'about' => 'pages#about'
+  get 'signup' => 'pages#signup'
+  get 'login' => 'pages#login'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
