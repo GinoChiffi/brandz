41f1453978020273d5d3ed6132f475c6d69e6d4d
Gino Chiffi <gino.chiffi@gmail.com>
2018-03-02 16:39:24 +0100

    fixed backbutton & route from stores index

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :brands
   devise_for :users
 
-  root 'pages#home'
+  root 'brands#index'
   get 'about' => 'pages#about'
   get 'signup' => 'pages#signup'
   get 'login' => 'pages#login'
